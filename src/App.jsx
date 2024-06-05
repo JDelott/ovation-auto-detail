b78c6d11ff88c56d00617f67ce6e57617d095b4b
Jacob Delott <jldelott@gmail.com>
2024-06-04 22:30:42 -0700

    added setup nav and darkmode feature

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  const [theme, setTheme] = React.useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+  const element = document.documentElement;
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+  return (
+    <div>
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  );
+};
+
+export default App;
