91761ba51f7155188888bebe70a9595995f89995
Jacob Delott <jldelott@gmail.com>
2024-06-07 11:02:51 -0700

    testimonial

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
+const TestimonialData = [
+  {
+    name: "Dilshad",
+    image: "",
+    description: "lorem ipsum dolor sit amet consecuteter adipisicing elit.",
+    aosDelay: "0",
+  },
+  {
+    name: "Satya",
+    image: "",
+    description: "lorem ipsum dolor sit amet consecuteter adipisicing elit.",
+    aosDelay: "300",
+  },
+  {
+    name: "Sabir",
+    image: "",
+    description: "lorem ipsum dolor sit amet consecuteter adipisicing elit.",
+    aosDelay: "100",
+  },
+];
+
 const Testimonial = () => {
   return (
     <div className="dark:bg-black dark:text-white py-14 sm:pb-24">
@@ -15,7 +36,18 @@ const Testimonial = () => {
             Lorem ipsom dolor sit amet consectutrut adidspsisc elit.
           </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-8"></div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 text-black dark:text-white"></div>
+        <div>
+          {TestimonialData.map((data) => {
+            return (
+              <div key={data.name}>
+                <div>
+                  <img src="https://picsum.photos/200" alt="" />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
