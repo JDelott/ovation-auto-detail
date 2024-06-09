910b8d75e2c6c67e84577df4d95241718afe020a
Jacob Delott <jldelott@gmail.com>
2024-06-08 19:57:20 -0700

    fixed nav for mobile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,67 +1,174 @@
-import React from "react";
-import { BiSolidMoon, BiSolidSun } from "react-icons/bi";
+// import React from "react";
+// import { BiSolidMoon, BiSolidSun } from "react-icons/bi";
 
-const Navlinks = [
+// const Navlinks = [
+//   {
+//     id: "1",
+//     name: "HOME",
+//     link: "/#",
+//   },
+//   {
+//     id: "2",
+//     name: "SERVICES",
+//     link: "/#cars",
+//   },
+//   {
+//     id: "3",
+//     name: "ABOUT",
+//     link: "/#about",
+//   },
+//   {
+//     id: "4",
+//     name: "BOOKING",
+//     link: "/#booking",
+//   },
+// ];
+
+// const Navbar = ({ theme, setTheme }) => {
+//   return (
+//     <nav className="shadow-md bg-white dark:bg-dark dark:text-white duration-300">
+//       <div className="container">
+//         <div className="flex justify-between items-center">
+//           <div>
+//             <h1 className="text-3xl font-sans">Ovation Auto Detail </h1>
+//           </div>
+//           <div className="hidden md:block">
+//             <ul className="flex items-center gap-8">
+//               {Navlinks.map(({ id, name, link }) => (
+//                 <li key={id} className="py-4">
+//                   <a
+//                     className="py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium"
+//                     href={link}
+//                   >
+//                     {name}
+//                   </a>
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//           <div>
+//             {theme === "dark" ? (
+//               <BiSolidSun
+//                 onClick={() => setTheme("light")}
+//                 className="text2xl"
+//               />
+//             ) : (
+//               <BiSolidMoon
+//                 onClick={() => setTheme("dark")}
+//                 className="text2xl"
+//               />
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+import React, { useState } from "react";
+import { BiSolidSun, BiSolidMoon } from "react-icons/bi";
+import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
+import ResponsiveMenu from "./ResponsiveMenu";
+
+export const Navlinks = [
   {
-    id: "1",
+    id: 1,
     name: "HOME",
     link: "/#",
   },
   {
-    id: "2",
-    name: "SERVICES",
+    id: 2,
+    name: "CARS",
     link: "/#cars",
   },
   {
-    id: "3",
+    id: 1,
     name: "ABOUT",
     link: "/#about",
   },
   {
-    id: "4",
+    id: 1,
     name: "BOOKING",
     link: "/#booking",
   },
 ];
-
 const Navbar = ({ theme, setTheme }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <nav className="shadow-md bg-white dark:bg-dark dark:text-white duration-300">
-      <div className="container">
+    <div
+      className="relative z-10 shadow-md w-full dark:bg-black dark:text-white duration-300
+    "
+    >
+      <div className="container py-2 md:py-0">
         <div className="flex justify-between items-center">
           <div>
-            <h1 className="text-3xl font-sans">Ovation Auto Detail </h1>
+            <span className="text-3xl font-normal">Ovation Auto Detail</span>
           </div>
-          <div className="hidden md:block">
+          <nav className="hidden md:block">
             <ul className="flex items-center gap-8">
               {Navlinks.map(({ id, name, link }) => (
                 <li key={id} className="py-4">
                   <a
-                    className="py-2 hover:border-b-2 hover:text-primary hover:border-primary transition-colors duration-500 text-lg font-medium"
                     href={link}
+                    className=" text-lg font-medium  hover:text-primary py-2 hover:border-b-2 hover:border-primary transition-colors duration-500  "
                   >
                     {name}
                   </a>
                 </li>
               ))}
+              {/* DarkMode feature implement */}
+              {theme === "dark" ? (
+                <BiSolidSun
+                  onClick={() => setTheme("light")}
+                  className="text-2xl"
+                />
+              ) : (
+                <BiSolidMoon
+                  onClick={() => setTheme("dark")}
+                  className="text-2xl"
+                />
+              )}
             </ul>
-          </div>
-          <div>
+          </nav>
+          {/* Mobile view  */}
+          <div className="flex items-center gap-4 md:hidden ">
+            {/* dark  mode */}
             {theme === "dark" ? (
               <BiSolidSun
                 onClick={() => setTheme("light")}
-                className="text2xl"
+                className="text-2xl"
               />
             ) : (
               <BiSolidMoon
                 onClick={() => setTheme("dark")}
-                className="text2xl"
+                className="text-2xl"
+              />
+            )}
+            {/* Mobile Hamburger icon */}
+            {showMenu ? (
+              <HiMenuAlt1
+                onClick={toggleMenu}
+                className=" cursor-pointer transition-all"
+                size={30}
+              />
+            ) : (
+              <HiMenuAlt3
+                onClick={toggleMenu}
+                className="cursor-pointer transition-all"
+                size={30}
               />
             )}
           </div>
         </div>
       </div>
-    </nav>
+      <ResponsiveMenu showMenu={showMenu} />
+    </div>
   );
 };
 
